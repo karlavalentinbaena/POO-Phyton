7bec73c573895869f0c375c9c1643a2426ea2472
Karla Valentin Baena <92067163+karlavalentinbaena@users.noreply.github.com>
2023-06-13 14:40:30 -0600

    Delete aritmetica.py

--- aritmetica.py
+++
@@ -1,9 +0,0 @@
-def suma(a, b):
-    if a != None and b !=None:
-        c = a + b
-    else:
-        print("Envia ambos valores")
-        c = 0
-    return c
-def resta(a = 0 , b = 1):
-    return a - b
